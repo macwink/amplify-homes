f672015cb41f77b7f2989899ec4603479e07daae
Wingky Faisal <wingky.faisal@outlook.com>
2022-02-08 18:37:38 +0100

    add paginated and width

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import { CardBCollection, NavBar, MarketingFooter } from "./ui-components";
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <CardBCollection />
-      <MarketingFooter />
+      <NavBar width={"100vw"} />
+      <CardBCollection isPaginated itemsPerPage={3} />
+      <MarketingFooter width={"100vw"} />
     </div>
   );
 }
